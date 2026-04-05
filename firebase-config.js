cc5d6881618326a4d73323d60749f8e2482b3e6b
wazzy604 <72707093+wazzy604@users.noreply.github.com>
2026-04-05 01:11:37 -0400

    Fix formatting of firebaseConfig object

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
-    const firebaseConfig = {
-        apiKey: "AIzaSyD9AlW7ggavCIKVgE6mHWHsPjJqu1g5hyE",
-        authDomain: "finance-tracker-c7ea4.firebaseapp.com",
-        projectId: "finance-tracker-c7ea4",
-        storageBucket: "finance-tracker-c7ea4.firebasestorage.app",
-        messagingSenderId: "120110205233",
-        appId: "1:120110205233:web:fbec76fecf6ab9cd660810"
-    };
+const firebaseConfig = {
+    apiKey: "AIzaSyD9AlW7ggavCIKVgE6mHWHsPjJqu1g5hyE",
+    authDomain: "finance-tracker-c7ea4.firebaseapp.com",
+    projectId: "finance-tracker-c7ea4",
+    storageBucket: "finance-tracker-c7ea4.firebasestorage.app",
+    messagingSenderId: "120110205233",
+    appId: "1:120110205233:web:fbec76fecf6ab9cd660810"
+};
